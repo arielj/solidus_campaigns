1395b60bb94a3e5446cfb9f1239a26ee62d33f9e
Ariel Juodziukynas <arieljuod@gmail.com>
2018-06-13 16:42:00 -0300

    fix migration

--- db/migrate/20180613192031_change_campaigns_status_default.rb
+++ db/migrate/20180613192031_change_campaigns_status_default.rb
@@ -1,5 +1,9 @@
 class ChangeCampaignsStatusDefault < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default :spree_campaigns, :status, 0
+  def self.up
+    change_column_default :spree_campaigns, :status, 1
+  end
+
+  def self.down
+    change_column_default :spree_campaigns, :status, nil
   end
 end
